67a78e4c12dd02c46b5dc659f558eea7402d743f
Sigurd Kvamme <sigkva97@lumigruppen.no>
2023-11-27 15:11:27 +0100

    update external link component

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -6,15 +6,21 @@ interface ExternalLinkProps {
   mode?: 'primary' | 'secondary';
 }
 
-export function ExternalLink({ href, children, mode = "primary" }: ExternalLinkProps) {
-
+export function ExternalLink({ href, children, mode = 'primary' }: ExternalLinkProps) {
   const modeClasses = {
     primary: 'bg-white text-neutral-900',
     secondary: 'bg-transparent text-white',
   };
 
   return (
-    <a className={twJoin(modeClasses[mode], "group flex gap-2 rounded-sm py-2 border border-white items-center justify-center w-32 transition-all hover:bg-purple-500 hover:border-purple-500 hover:text-white")} href={href} target='_blank'>
+    <a
+      className={twJoin(
+        modeClasses[mode],
+        'group flex gap-2 rounded-sm py-2 border border-white items-center justify-center w-32 transition-all hover:bg-purple-500 hover:border-purple-500 hover:text-white'
+      )}
+      href={href}
+      target='_blank'
+      rel='noreferer'>
       {children}
     </a>
   );
